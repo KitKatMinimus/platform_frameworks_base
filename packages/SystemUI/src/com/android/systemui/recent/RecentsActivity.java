6d8f5c1c4d67311b5afc14210f3c9609c661f2e1
drcmda <drcmda@gmail.com>
2014-03-25 10:53:31 -0400

    mend

--- packages/SystemUI/src/com/android/systemui/recent/RecentsActivity.java
+++ packages/SystemUI/src/com/android/systemui/recent/RecentsActivity.java
@@ -32,6 +32,7 @@ import android.view.WindowManager;
 import com.android.systemui.R;
 import com.android.systemui.statusbar.StatusBarPanel;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class RecentsActivity extends Activity {
@@ -45,6 +46,10 @@ public class RecentsActivity extends Activity {
     private static final String WAS_SHOWING = "was_showing";
 
     private RecentsPanelView mRecentsPanel;
+
+    private static List<NavigationCallback> mNavigationCallbacks;
+    private static RecentsPanelView mRecentsPanel;
+    private static boolean mShowing;
     private IntentFilter mIntentFilter;
     private boolean mShowing;
     private boolean mForeground;
@@ -115,6 +120,17 @@ public class RecentsActivity extends Activity {
 
     public static boolean forceOpaqueBackground(Context context) {
         return WallpaperManager.getInstance(context).getWallpaperInfo() != null;
+    }
+
+    public void setRecentHints(boolean show) {
+        for(NavigationCallback callback : mNavigationCallbacks) {
+            // Check if we need to enable alternate drawable for recent apps key
+            if(callback == null) return; // Multiuser is not allowed
+            int navigationHints = callback.getNavigationIconHints();
+            callback.setNavigationIconHints(NavigationBarView.NAVBAR_RECENTS_HINT,
+                    show ? (navigationHints | StatusBarManager.NAVIGATION_HINT_RECENT_ALT)
+                            : (navigationHints & ~StatusBarManager.NAVIGATION_HINT_RECENT_ALT), true);
+        }
     }
 
     @Override
@@ -241,7 +257,17 @@ public class RecentsActivity extends Activity {
         return mForeground;
     }
 
-    boolean isActivityShowing() {
-         return mShowing;
+    public static void addNavigationCallback(NavigationCallback callback) {
+        if(mNavigationCallbacks == null)
+                mNavigationCallbacks= new ArrayList<NavigationCallback>();
+        mNavigationCallbacks.add(callback);
+    }
+
+    public static int getTasks() {
+        return mRecentsPanel.getTasks();
+    }
+
+    public static boolean isActivityShowing() {
+        return mShowing;
     }
 }
